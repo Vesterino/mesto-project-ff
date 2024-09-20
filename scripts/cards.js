0ec2be00f3a8e470c8e2e7d6b6d9cdc2db910ced
etozheVester <eto.zhe.vester@gmail.com>
2024-09-20 17:32:52 +0300

    add project

---
+++ scripts/cards.js
@@ -0,0 +1,26 @@
+const initialCards = [
+    {
+      name: "Олимпик",
+      link: "./images/card_1.jpg",
+    },
+    {
+      name: "Иннерткирхен",
+      link: "./images/card_2.jpg",
+    },
+    {
+      name: "Пустыня",
+      link: "./images/card_3.jpg",
+    },
+    {
+      name: "Ергаки",
+      link: "./images/card_4.jpg",
+    },
+    {
+      name: "Собор Святого Павла",
+      link: "./images/card_5.jpg",
+    },
+    {
+      name: "Италия",
+      link: "./images/card_6.jpg",
+    }
+];
---
+++ scripts/index.js
@@ -0,0 +1,35 @@
+// @todo: Темплейт карточки
+
+const cardTemplate = document.querySelector('#card-template').content;
+
+// @todo: DOM узлы
+
+const cardsList = document.querySelector('.places__list');
+
+// @todo: Функция создания карточки
+
+function createCard(card, deleteCard) {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    
+    cardElement.querySelector('.card__title').textContent = card.name;
+    const cardImage = cardElement.querySelector('.card__image');
+    cardImage.src = card.link;
+    cardImage.alt = card.name;
+
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', () => deleteCard(cardElement));
+    
+    return cardElement;
+};
+
+// @todo: Функция удаления карточки
+
+function deleteCard(card) {
+    card.remove()
+};
+
+// @todo: Вывести карточки на страницу
+
+initialCards.forEach(function(card) {
+    cardsList.append(createCard(card, deleteCard));
+});
