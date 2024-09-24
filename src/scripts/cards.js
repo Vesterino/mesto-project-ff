85d902e6bd1335ba29a823cbbc29d106ed70f4e5
etozheVester <eto.zhe.vester@gmail.com>
2024-09-25 01:10:51 +0300

    add webpack

---
+++ src/scripts/cards.js
@@ -0,0 +1,26 @@
+const initialCards = [
+    {
+      name: "Осень",
+      link: "https://unsplash.com/photos/background-pattern-zKAYpLmPHdc",
+    },
+    {
+      name: "Галата",
+      link: "https://unsplash.com/photos/a-view-of-a-city-with-a-clock-tower-QC8d5U4Iaek",
+    },
+    {
+      name: "Глюфрабуй",
+      link: "https://unsplash.com/photos/a-person-standing-in-front-of-a-waterfall-dN8vU0QiuRQ",
+    },
+    {
+      name: "Ландманналаугар",
+      link: "https://unsplash.com/photos/a-view-of-a-mountain-with-a-road-going-through-it-vOTKM48Llzc",
+    },
+    {
+      name: "Чикаго",
+      link: "https://unsplash.com/photos/a-city-street-at-night-with-a-theater-sign-lit-up-5H6iRygQO7U",
+    },
+    {
+      name: "Подсолнух",
+      link: "https://unsplash.com/photos/a-large-sunflower-in-a-field-of-sunflowers-eYLAzx1oBAU",
+    }
+];
