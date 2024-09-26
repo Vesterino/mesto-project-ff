9eb4d11fd43ab1eacccb5686cdb7fc1138478f71
etozheVester <eto.zhe.vester@gmail.com>
2024-09-26 17:42:53 +0300

    complete popup && add function edit profile

--- src/index.js
+++ src/index.js
@@ -42,51 +42,99 @@ initialCards.forEach(function(card) {
     cardsList.append(createCard(card, deleteCard));
 });
 
-// Popup
+// Попапы
 
 const popup = document.querySelector('.popup');
 const popupEdit = document.querySelector('.popup_type_edit');
 const popupNewCard = document.querySelector('.popup_type_new-card');
 const popupImage = document.querySelector('.popup_type_image');
 
-// Button popup
+// Кнопки попапов
 
 const buttonEditProfile = document.querySelector('.profile__edit-button');
 const buttonNewCard = document.querySelector('.profile__add-button');
 const buttonImage = document.querySelector('.card__image');
 const buttonClosePopup = document.querySelectorAll('.popup__close');
 
+// Анимация попапов
+
+popupEdit.classList.add('popup_is-animated');
+popupNewCard.classList.add('popup_is-animated');
+popupImage.classList.add('popup_is-animated');
+
+// Функции открытия каждого попапа
+
 function openPopupEdit() {
-    popupEdit.style.display = 'flex';
+    popupEdit.classList.add('popup_is-opened');
 }
 
 function openPopupNewCard() {
-    popupNewCard.style.display = 'flex';
+    popupNewCard.classList.add('popup_is-opened');
 }
 
 function openPopupImage() {
-    popupImage.style.display = 'flex';
+    popupImage.classList.add('popup_is-opened');
 }
 
-buttonEditProfile.addEventListener('click', openPopupEdit);
+// Слушатели каждого попапа
+
+buttonEditProfile.addEventListener('click', () => {
+    inputNameProfile.value = titleProfile.textContent;
+    inputDescriptionProfile.value = descriptionProfile.textContent;
+    openPopupEdit();
+});
+
 buttonNewCard.addEventListener('click', openPopupNewCard);
 buttonImage.addEventListener('click', openPopupImage);
 
+// Функция закрытия попапа
+
 function closePopup(popup) {
-    popup.style.display = 'none';
+    popup.classList.remove('popup_is-opened');
 }
 
 buttonClosePopup.forEach(function(button) {
     button.addEventListener('click', () => {
-        if(popupEdit.style.display === 'flex') {
+        if(popupEdit.classList.contains('popup_is-opened')) {
             closePopup(popupEdit);
-        } else if(popupNewCard.style.display === 'flex') {
+        } else if(popupNewCard.classList.contains('popup_is-opened')) {
             closePopup(popupNewCard);
-        } else if(popupImage.style.display === 'flex') {
+        } else if(popupImage.classList.contains('popup_is-opened')) {
             closePopup(popupImage);
         }
     });
 });
+
+popup.addEventListener('click', evt => {
+    if (evt.target.classList.contains('popup')) {
+        closePopup(evt.target.closest('.popup'));
+    }
+});
+
+document.addEventListener('keydown', evt => {
+    if (evt.key === 'Escape') {
+        const openedPopup = document.querySelector('.popup_is-opened');
+        closePopup(openedPopup);
+    }
+});
+
+// Редактирование профиля
+
+const formElement = document.forms.editProfile;
+const inputNameProfile = formElement.name;
+const inputDescriptionProfile = formElement.description;
+const titleProfile = document.querySelector('.profile__title');
+const descriptionProfile = document.querySelector('.profile__description');
+
+function editProfile(evt) {
+    evt.preventDefault();
+    titleProfile.textContent = inputNameProfile.value;
+    descriptionProfile.textContent = inputDescriptionProfile.value;
+    closePopup(popupEdit);
+}
+
+formElement.addEventListener('submit', editProfile);
+
 
 
 // Like
--- src/scripts/cards.js
+++ src/scripts/cards.js
@@ -1,26 +1,26 @@
 export const initialCards = [
     {
       name: "Осень",
-      link: "https://unsplash.com/photos/background-pattern-zKAYpLmPHdc",
+      link: "https://images.unsplash.com/photo-1659363649792-798161d97246?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
       name: "Галата",
-      link: "https://unsplash.com/photos/a-view-of-a-city-with-a-clock-tower-QC8d5U4Iaek",
+      link: "https://images.unsplash.com/photo-1726808260756-ec1d4eceaf71?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
       name: "Глюфрабуй",
-      link: "https://unsplash.com/photos/a-person-standing-in-front-of-a-waterfall-dN8vU0QiuRQ",
+      link: "https://images.unsplash.com/photo-1726677644019-c010b789cf12?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
       name: "Ландманналаугар",
-      link: "https://unsplash.com/photos/a-view-of-a-mountain-with-a-road-going-through-it-vOTKM48Llzc",
+      link: "https://images.unsplash.com/photo-1726533862204-8110e6193fe9?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
       name: "Чикаго",
-      link: "https://unsplash.com/photos/a-city-street-at-night-with-a-theater-sign-lit-up-5H6iRygQO7U",
+      link: "https://images.unsplash.com/photo-1726458573518-04a433641cb4?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
       name: "Подсолнух",
-      link: "https://unsplash.com/photos/a-large-sunflower-in-a-field-of-sunflowers-eYLAzx1oBAU",
+      link: "https://images.unsplash.com/photo-1726486896376-4d1340e2f672?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     }
 ];
